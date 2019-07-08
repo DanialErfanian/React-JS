b3909b8b9eb2ffb9758910be5aed1cb5107ad101
DanialErfanian <erfaniiyanerfan@gmail.com>
2019-07-08 08:24:46 +0430

    fix cells active protperty

--- Shalamrood/src/components/App.js
+++ Shalamrood/src/components/App.js
@@ -17,12 +17,70 @@ class App extends React.Component {
 
 
     render() {
+        setCellActive(this.levels[this.state.level]);
+        console.log("rendering App");
+        console.log(this.levels[this.state.level]);
         return <div className="game-container">
             <div className="game">
                 <Menu level={this.state.level}/>
                 <Board rows={this.state.rows} cols={this.state.cols} cells={this.state.cells}/>
             </div>
         </div>;
+    }
+}
+
+let NEIGHBORS = {
+    "L": [0, 3],
+    "I": [1, 3],
+    "T": [0, 1, 2],
+    "P": [0, 1, 2, 3],
+    "C": [3],
+};
+
+function getNeighbor(level, i, direction) {
+    if (direction === 0)
+        return i + 1;
+    if (direction === 1)
+        return i + level.cols;
+    if (direction === 3)
+        return i - level.cols;
+    return i - 1;
+}
+
+function getConnectedNeighbors(level, i) {
+    let list = NEIGHBORS[level.cells[i].type];
+    let res = [];
+    list.forEach((x) => {
+        res.push(getNeighbor(level, i, (x + level.cells[i].rotate) % 4));
+    });
+    return res;
+}
+
+
+function setCellActive(level) {
+    let cells = level.cells;
+    let source = level.source;
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].active = i === source;
+    }
+    let updated = true;
+    while (updated) {
+        updated = false;
+        for (let i = 0; i < cells.length; i++) {
+            if (!cells[i].active) {
+                for (let j of getConnectedNeighbors(level, i)) {
+                    if (i === 7)
+                        console.log(j);
+                    if (j < 0 || j >= cells.length)
+                        continue;
+                    if (cells[j].active && getConnectedNeighbors(level, j).includes(i)) {
+                        cells[i].active = true;
+                        updated = true;
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
 
